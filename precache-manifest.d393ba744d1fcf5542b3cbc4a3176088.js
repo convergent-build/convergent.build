acb4b368a8adefe516ec1c3f8856f416338b4fd6
rainiera <rainier@utexas.edu>
2019-04-24 00:21:31 -0700

    v2.0

---
+++ precache-manifest.d393ba744d1fcf5542b3cbc4a3176088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90178aaa7b7d88861286ca1c201b297",
+    "url": "./index.html"
+  },
+  {
+    "revision": "05c1dcde262dbbf09e72",
+    "url": "./static/css/main.c15d9f20.chunk.css"
+  },
+  {
+    "revision": "e0bbfd8b6ad3b44c8a18",
+    "url": "./static/js/2.99abb545.chunk.js"
+  },
+  {
+    "revision": "05c1dcde262dbbf09e72",
+    "url": "./static/js/main.e8626891.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
